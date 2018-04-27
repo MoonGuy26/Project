f9ec3ee4007286e36eedde8ac12ed97f87e4aa27
Sinan Daroukh <s.daroukh@live.fr>
2018-04-27 18:18:33 +0200

    Adding new kinds of views : orderings views

--- Beanify/Beanify/ViewModels/OrderNewViewModel.cs
+++ Beanify/Beanify/ViewModels/OrderNewViewModel.cs
@@ -2,6 +2,7 @@
 using Beanify.Views;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
@@ -10,13 +11,34 @@ namespace Beanify.ViewModels
 {
     public class OrderNewViewModel : BaseViewModel
     {
-        private AccountService _accountService;
+        private IAccountService _accountService;
+
+        private ObservableCollection<int> _observable_picker;
+
+        public ObservableCollection<int> ObservablePicker
+        {
+            get { return _observable_picker;  }
+        }
+
+        static private int capacity = 100;
+        private List<int> picker_list = new List<int>(capacity);
 
 
+        private List<int> ListInitializer(List<int> list)
+        {
+            for (int i = 1; i <= capacity; i++)
+            {
+                list.Add(i);
+            }
+            return list;
+        }
 
         public OrderNewViewModel(IAccountService accountService) : base()
         {
-            _accountService = new AccountService();
+            _accountService = accountService;
+
+            picker_list = ListInitializer(picker_list);
+            _observable_picker = new ObservableCollection<int>(picker_list);
 
             Commands.Add("Continue", new Command(OnContinueExecute));
         }
@@ -29,6 +51,7 @@ namespace Beanify.ViewModels
         {
             await _navigationService.NavigateToAsync<OrderReviewViewModel>();
         }
+
 
 
     }
